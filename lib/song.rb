df6d18124aa3ad990618475121488bb2f37b928b
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-30 17:58:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,18 @@
 class Song
-  attr_accessor :artist, :name
+  attr_accessor :name, :artist
+  @@all = []
 
   def initialize(name)
     @name = name
+    save
   end
 
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def self.new_by_filename(file)
